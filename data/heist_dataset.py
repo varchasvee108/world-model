ceee255116551541958365024d1d553e712bce0a
varchasvi <varchasvee108@gmail.com>
2026-04-19 11:27:39 +0530

    dataset code

---
+++ data/heist_dataset.py
@@ -0,0 +1,33 @@
+from datasets import load_dataset
+from torch.utils.data import Dataset
+import torchvision.transforms as T
+import torch
+from core.config import Config
+
+
+class HeistDataset(Dataset):
+    def __init__(self, config: Config, split: str):
+        ds = load_dataset(config.data.dataset, name="heist", split=split)
+        self.frames = ds["image"]
+        self.actions = ds["action"]
+        self.tf = T.Compose(
+            [
+                T.Resize(config.data.image_size),
+                T.ToTensor(),
+                T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
+
+    def __len__(self):
+        return len(self.frames) - 1
+
+    def __getitem__(self, i):
+        frame_t = self.tf(self.frames[i])
+        frame_t_plus_1 = self.tf(self.frames[i + 1])
+        action = self.actions[i]
+
+        action_map = torch.zeros(15, 64, 64)
+        action_map[action] = 1.0
+
+        cond = torch.cat([frame_t, action_map], dim=0)
+        return cond, frame_t_plus_1
